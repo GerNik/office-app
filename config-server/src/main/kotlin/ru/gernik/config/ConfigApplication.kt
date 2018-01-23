07d84cd2da05982edb4fa404c7ee2892762c0869
Nikolay Gerasimov <ngerasimov2@alfabank.ru>
2018-01-23 16:54:27 +0300

    added config-server application

---
+++ config-server/src/main/kotlin/ru/gernik/config/ConfigApplication.kt
@@ -0,0 +1,13 @@
+package ru.gernik.config
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@SpringBootApplication
+@EnableConfigServer
+open class ConfigApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ConfigApplication::class.java, *args)
+}
